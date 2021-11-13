7b1274a559b6f23705587ee551aa4e8b605911a5
zhangxinzhou <xiaoxinerror@live.cn>
2021-11-13 13:38:29 +0800

    111

--- cpp_learn/zxz/add.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int add(int a, int b)
-{
-    printf("%d + %d = %d\n", a, b, a + b);
-    return 0;
-}
--- cpp_learn/zxz/main.c
+++ cpp_learn/zxz/main.c
@@ -1,6 +1,16 @@
-#include <add.h>
+#include <stdio.h>
+int count = 0;
+int value;
+void func(int sum)
+{
+    printf("sum is :%d\n", sum);
+}
 
 int main()
 {
-    add(2, 3);
+    static int a = 1;
+    static int b = 0;
+    int x = 1;
+    func(a + b + x);
+    return 0;
 }
